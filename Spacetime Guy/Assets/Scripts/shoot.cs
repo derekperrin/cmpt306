f628bc643cea8072c1818b504ee6f4160b06df6d
Jesse-School\Jesse <jrr867@mail.usask.ca>
2017-10-01 12:39:42 -0600

    Fixed shooting code (shoots correctly in all 4 ways now!) added comments to make it clearer what each of the variables mean and are used for, and also made some slight changes to the control scheme/inputs

--- Spacetime Guy/Assets/Scripts/shoot.cs
+++ Spacetime Guy/Assets/Scripts/shoot.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class shoot : MonoBehaviour {
 
     public Rigidbody2D bulletAsset;
+    //this is how fast you can fire
     public float fireSpeed;
+    //this is the cooldown/delay of the weapon b/w shots
     public float weaponCooldown;
+    //this is how fast the bullet actually FLIES.
     public float bulletSpeed;
     public float xOffset;
     //these are used to make it look like it's shot from whatever it needs to be shot
@@ -25,18 +28,25 @@ public class shoot : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Time.time >= weaponCooldown)
-        {
-            if (Input.GetButton("Fire+X") || Input.GetButton("Fire-X") || Input.GetButton("Fire+Y") || Input.GetButton("Fire-Y")) { 
-                Fire();
+        if (Time.time >= weaponCooldown){
+            if (Input.GetButton("FireX"))  {
+                float shootDirX = Input.GetAxisRaw("FireX");
+                Vector2 xMoveVec = new Vector2((shootDirX*50) * bulletSpeed, 0);
+                Fire(xMoveVec);
+            } else if (Input.GetButton("FireY")){
+               float shootDirY = Input.GetAxisRaw("FireY");
+                Vector2 yMoveVec = new Vector2(0,(shootDirY*50) * bulletSpeed);
+                Fire(yMoveVec);
             }
         }
+        
     }
 
-    void Fire() {
-        Rigidbody2D bPrefab = Instantiate(bulletAsset, new Vector3(transform.position.x + xOffset, transform.position.y + YOffset, transform.position.z), Quaternion.identity) as Rigidbody2D;
-        //bPrefab.rigidbody2D.AddForce(Vector2.up * bulletSpeed);
-        bPrefab.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bulletSpeed);
+    void Fire(Vector2 direction) {
+        
+  Rigidbody2D bPrefab = Instantiate(bulletAsset, new Vector3(transform.position.x + xOffset, transform.position.y + YOffset, transform.position.z), Quaternion.identity) as Rigidbody2D;
+        //bPrefab.rigidbody2D.AddForce(Vector2.up * bulletSpeed); 
+        bPrefab.GetComponent<Rigidbody2D>().AddForce(direction);
             //.addForce(Vector2.up * bulletSpeed);
         weaponCooldown = Time.time + fireSpeed;
 	}
