eb1155a9634bceb00a6b60ef2959befc67aad0a2
Dominic McKeith <dominic.mckeith@usask.ca>
2017-10-14 15:25:08 -0600

    Refactored code from hero.cs into two new scripts, character.cs and player.cs that utilize inheritance. hero.cs has been deleted as the functionality is now is character.cs and player.cs. The "hero" now contains the player.cs script instead of hero.cs
    Currently, the player acts exactly the same as it did before.
    
    Details:
    charatcer.cs contains an abstract class with some abstract methods that must be implemented in child classes.
    player.cs is a child class of character.cs. In the future when we implement enemies, each enemy will be a
    child class of character.cs. This means that enemies will have the same basic implementation as the character.
    The difference will be how they get input and how they move. The player will get input from the keyboard while
    the enemies will get input through some other method that will determine how to move.

---
+++ Spacetime Guy/Assets/Scripts/characters/character.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class character : MonoBehaviour
+{
+
+    protected Rigidbody2D heroRigidBody;
+    [SerializeField]
+    protected float speed;
+
+    protected abstract void Start();
+
+    /***
+     * GetInput(ref float movementX, ref float movementY)
+     * 
+     * Get the character input and place the x movement in movementX and place the y movement in movementY
+     */
+    protected abstract void GetInput(ref float movementX, ref float movementY);
+
+    /***
+     * Movement(float horizonal, float vertical)
+     * 
+     * Move the character.
+     */
+    protected abstract void Movement(float horizontal, float vertical);
+
+    /***
+     * Move the character
+     */
+    protected void FixedUpdate()
+    {
+        float movementX = 0;
+        float movementY = 0;
+        GetInput(ref movementX, ref movementY);
+        Movement(movementX, movementY);
+    }
+
+    
+
+}
---
+++ Spacetime Guy/Assets/Scripts/characters/player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : character {
+
+    protected override void Start()
+    {
+        heroRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    protected override void Movement(float horizontal, float vertical)
+    {
+        Vector2 movement = new Vector2(horizontal, vertical);
+
+        heroRigidBody.velocity = new Vector2(horizontal * speed, heroRigidBody.velocity.y);
+        heroRigidBody.velocity = new Vector2(heroRigidBody.velocity.x, vertical * speed);
+    }
+
+    protected override void GetInput(ref float movementX, ref float movementY)
+    {
+        movementX = Input.GetAxis("Horizontal");  //A(negative) D(positive) arrow keys 
+        movementY = Input.GetAxis("Vertical");  //S(negative) W(positive) arrow keys 
+    }
+}
