876d927745ff73db8f2a292c631766e22d611a4c
Jesse-School\Jesse <jrr867@mail.usask.ca>
2017-11-04 16:02:11 -0600

    Bugfixing Hallways; works better but still missing hallways

--- Spacetime Guy/Assets/Scripts/Hallways.cs
+++ Spacetime Guy/Assets/Scripts/Hallways.cs
@@ -5,64 +5,153 @@ using UnityEngine;
 
 
 public class Hallways {
-    //bottom left corner point
-    public int xCornerStarting;
-    public int yCornerStarting;
-    //ending room corner point
-    public int xCornerEnding;
-    public int yCornerEnding;
+    //bottom left corner point of starting room
+    public int startRoomXCorner;
+    public int startRoomYCorner;
+    //bottom left conrner point of ending room ending room
+    public int endRoomXCorner;
+    public int endRoomYCorner;
+    public enum Direction { NORTH, EAST, SOUTH, WEST,ERROR };
 
     public int hallwayHorizontalLength = 0;
     public int hallwayVerticalLength = 0;
 
 
-    public int hallwayWidth = 1;
-   
+    public int hallwayWidth = 5;
+
     //Two rooms we're connecting
     public Room startingRoom;
     public Room endingRoom;
 
     //the point we need to bend to create an L-shape 
-    public int xBendCorner=0;
-    public int yBendCorner=0;
+    public int xBendCorner = 0;
+    public int yBendCorner = 0;
 
     //tells if we need to bend the hallway.
-    bool lShape = false;
+    //bool lShape = false;
 
-    
-    bool hallwayIntersect = false;
+
+    //bool hallwayIntersect = false;
     //boolean top = false;
     //boolean left = false;
     //how much they overlap within the distance between each other.
-    int xOverlap;
-    int yOverlap;
+    //int xOverlap;
+    //int yOverlap;
 
     public Hallways(Room startingRoom, Room endingRoom)
     {
-        if (Mathf.Abs(startingRoom.xPos - endingRoom.xPos) > Mathf.Abs(startingRoom.yPos - endingRoom.yPos))
+        Direction currDir = GetDirection(startingRoom, endingRoom);
+        Debug.Log(currDir);
+        if (currDir == Direction.WEST)
+        {
+            hallwayHorizontalLength = ((endingRoom.xPos + endingRoom.roomWidth) - startingRoom.xPos) - 1;
+
+
+            startRoomXCorner = startingRoom.xPos;
+            startRoomYCorner = startingRoom.yPos + (endingRoom.roomHeight / 2);
+            endRoomXCorner = endingRoom.xPos + endingRoom.roomWidth;
+            endRoomYCorner = startRoomYCorner;
+            // extra stuff for L-shaped hallways and stuff here: Won't need to happen w/ current iteration of code.
+        }
+        else if (currDir == Direction.EAST)
+        {
+            //Hallway is going to the right
+            hallwayHorizontalLength = (endingRoom.xPos - (startingRoom.xPos + startingRoom.roomWidth));
+
+            startRoomXCorner = startingRoom.xPos + startingRoom.roomWidth;
+            startRoomYCorner = startingRoom.yPos + endingRoom.roomHeight / 2;
+
+
+            endRoomXCorner = startingRoom.xPos;
+            endRoomYCorner = startRoomYCorner;
+            // extra stuff for L-shaped hallways and stuff here: Won't need to happen w/ current iteration of code.
+        }
+        else if (currDir == Direction.SOUTH)
+        {
+            hallwayVerticalLength = ((endingRoom.yPos + endingRoom.roomHeight) - startingRoom.yPos);
+
+            startRoomYCorner = startingRoom.yPos;
+            startRoomXCorner = startingRoom.xPos + endingRoom.roomWidth / 2;
+            
+            
+            endRoomYCorner = (endingRoom.yPos + endingRoom.roomHeight);
+            endRoomXCorner = startRoomXCorner;
+            // extra stuff for L-shaped hallways and stuff here: Won't need to happen w/ current iteration of code.
+        }
+        else if (currDir == Direction.NORTH)
+        {
+            hallwayVerticalLength = (endingRoom.yPos - (startingRoom.yPos + startingRoom.roomHeight));
+            
+            startRoomYCorner = startingRoom.yPos + startingRoom.roomHeight;
+            startRoomXCorner = startingRoom.xPos + endingRoom.roomWidth / 2;
+            
+            
+            endRoomYCorner = endingRoom.yPos;
+            endRoomXCorner = startRoomXCorner;
+        } else
+        {
+            Debug.Log("Your Direction function created an error somewhere and you are bad!");
+        }
+    }
+    //gets the direction of the two rooms relative to each other.
+    public Direction GetDirection(Room room1,Room room2)
+    {
+        //going left/right
+        if (Mathf.Abs(room1.xPos - room2.xPos) >= Mathf.Abs(room1.yPos - room2.yPos))
+        {
+            if ((room1.xPos - room2.xPos) >= 0)
+            {
+                return Direction.WEST;
+            }
+            else
+            {
+                return Direction.EAST;
+            }
+        }
+        //going up/down
+        else if (Mathf.Abs(room1.xPos - room2.xPos) < Mathf.Abs(room1.yPos - room2.yPos))
+        {
+            if (room1.yPos - room2.yPos >= 0)
+            {
+                return Direction.SOUTH;
+            }
+            else
+            {
+                return Direction.NORTH;
+            }
+        }
+        else
+        {
+            Debug.Log("Error this code is bad and does not work!");
+            return Direction.ERROR;
+        }
+    }
+    
+}
+     /**   if (Mathf.Abs(startingRoom.xPos - endingRoom.xPos) > Mathf.Abs(startingRoom.yPos - endingRoom.yPos))
         {
             if (startingRoom.xPos - endingRoom.xPos > 0)
             {
                 //Hallway is going to the left
-                xCornerStarting = startingRoom.xPos;
-                yCornerStarting = startingRoom.yPos + startingRoom.roomHeight / 2;
-                //xCornerEnding = endingRoom.xPos;
+                startRoomXCorner = startingRoom.xPos;
+                startRoomYCorner = startingRoom.yPos + startingRoom.roomHeight / 2;
+                //endRoomXCorner = endingRoom.xPos;
                 hallwayHorizontalLength = (endingRoom.xPos + endingRoom.roomWidth) - startingRoom.xPos;
-                xCornerEnding = startingRoom.xPos + hallwayHorizontalLength;
-                yCornerEnding = yCornerStarting;
+                endRoomXCorner = startingRoom.xPos + hallwayHorizontalLength;
+                endRoomYCorner = startRoomYCorner;
                 //check if room and hallway overlap
-                if (!CheckIfHallwayOverlap(endingRoom, xCornerEnding, yCornerEnding))
+                if (!CheckIfHallwayOverlap(endingRoom, endRoomXCorner, endRoomYCorner))
                 {
                     lShape = true;
-                    xBendCorner = xCornerEnding - 1;
-                    yBendCorner = yCornerEnding;
+                    xBendCorner = endRoomXCorner - 1;
+                    yBendCorner = endRoomYCorner;
                     hallwayHorizontalLength--;
 
                     if (lShape == true)
                     {
                         int bendDir = WhichWayToBend(endingRoom, xBendCorner, yBendCorner);
-                        xCornerEnding = xBendCorner;
-                        yCornerEnding = yBendCorner + (bendDir * Mathf.Abs(endingRoom.yPos - yBendCorner));
+                        endRoomXCorner = xBendCorner;
+                        endRoomYCorner = yBendCorner + (bendDir * Mathf.Abs(endingRoom.yPos - yBendCorner));
                         hallwayVerticalLength = endingRoom.yPos - yBendCorner;
                     }
                 }
@@ -71,25 +160,25 @@ public class Hallways {
             {
 
 
-                xCornerStarting = startingRoom.xPos + startingRoom.roomWidth;
-                yCornerStarting = startingRoom.yPos + startingRoom.roomHeight / 2;
+                startRoomXCorner = startingRoom.xPos + startingRoom.roomWidth;
+                startRoomYCorner = startingRoom.yPos + startingRoom.roomHeight / 2;
                 //Hallway is going to the right
                 hallwayHorizontalLength = endingRoom.xPos - startingRoom.xPos + startingRoom.roomWidth;
-                xCornerEnding = startingRoom.xPos + hallwayHorizontalLength;
-                yCornerEnding = yCornerStarting;
+                endRoomXCorner = startingRoom.xPos + hallwayHorizontalLength;
+                endRoomYCorner = startRoomYCorner;
                 //check if room and hallway overlap
-                if (!CheckIfHallwayOverlap(endingRoom, xCornerEnding, yCornerEnding))
+                if (!CheckIfHallwayOverlap(endingRoom, endRoomXCorner, endRoomYCorner))
                 {
                     lShape = true;
-                    xBendCorner = xCornerEnding + 1;
-                    yBendCorner = yCornerEnding;
+                    xBendCorner = endRoomXCorner + 1;
+                    yBendCorner = endRoomYCorner;
                     hallwayHorizontalLength++;
 
                     if (lShape == true)
                     {
                         int bendDir = WhichWayToBend(endingRoom, xBendCorner, yBendCorner);
-                        xCornerEnding = xBendCorner;
-                        yCornerEnding = yBendCorner + (bendDir * Mathf.Abs(endingRoom.yPos - yBendCorner));
+                        endRoomXCorner = xBendCorner;
+                        endRoomYCorner = yBendCorner + (bendDir * Mathf.Abs(endingRoom.yPos - yBendCorner));
                         hallwayVerticalLength = endingRoom.yPos - yBendCorner;
                     }
                 }
@@ -100,50 +189,50 @@ public class Hallways {
             if (startingRoom.yPos - endingRoom.yPos > 0)
             {
                 //Hallway going down
-                yCornerStarting = startingRoom.yPos;
-                xCornerStarting = startingRoom.xPos + startingRoom.roomWidth / 2;
-                //xCornerEnding = endingRoom.xPos;
+                startRoomYCorner = startingRoom.yPos;
+                startRoomXCorner = startingRoom.xPos + startingRoom.roomWidth / 2;
+                //endRoomXCorner = endingRoom.xPos;
                 hallwayVerticalLength = (endingRoom.yPos + endingRoom.roomHeight)-startingRoom.yPos;
-                yCornerEnding = startingRoom.yPos + hallwayVerticalLength;
-                xCornerEnding = xCornerStarting;
+                endRoomYCorner = startingRoom.yPos + hallwayVerticalLength;
+                endRoomXCorner = startRoomXCorner;
                 //check if room and hallway overlap
-                if (!CheckIfHallwayOverlap(endingRoom, xCornerEnding, yCornerEnding))
+                if (!CheckIfHallwayOverlap(endingRoom, endRoomXCorner, endRoomYCorner))
                 {
                     lShape = true;
-                    yBendCorner = yCornerEnding - 1;
-                    xBendCorner = xCornerEnding;
+                    yBendCorner = endRoomYCorner - 1;
+                    xBendCorner = endRoomXCorner;
                     hallwayVerticalLength--;
 
                     if (lShape == true)
                     {
                         int bendDir = WhichWayToBend(endingRoom, xBendCorner, yBendCorner);
-                        yCornerEnding = yBendCorner;
-                        xCornerEnding = xBendCorner + (bendDir * Mathf.Abs(endingRoom.xPos - xBendCorner));
+                        endRoomYCorner = yBendCorner;
+                        endRoomXCorner = xBendCorner + (bendDir * Mathf.Abs(endingRoom.xPos - xBendCorner));
                         hallwayHorizontalLength = endingRoom.xPos - xBendCorner;
                     }
                 }
                 else
                 {
                     //Hallway going up
-                    yCornerStarting = startingRoom.yPos + startingRoom.roomHeight;
-                    xCornerStarting = startingRoom.xPos + startingRoom.roomWidth / 2;
-                    //xCornerEnding = endingRoom.xPos;
+                    startRoomYCorner = startingRoom.yPos + startingRoom.roomHeight;
+                    startRoomXCorner = startingRoom.xPos + startingRoom.roomWidth / 2;
+                    //endRoomXCorner = endingRoom.xPos;
                     hallwayVerticalLength = endingRoom.yPos - (startingRoom.yPos + startingRoom.roomHeight);
-                    yCornerEnding = startingRoom.yPos + hallwayVerticalLength;
-                    xCornerEnding = xCornerStarting;
+                    endRoomYCorner = startingRoom.yPos + hallwayVerticalLength;
+                    endRoomXCorner = startRoomXCorner;
                     //check if room and hallway overlap
-                    if (!CheckIfHallwayOverlap(endingRoom, xCornerEnding, yCornerEnding))
+                    if (!CheckIfHallwayOverlap(endingRoom, endRoomXCorner, endRoomYCorner))
                     {
                         lShape = true;
-                        yBendCorner = yCornerEnding + 1;
-                        xBendCorner = xCornerEnding;
+                        yBendCorner = endRoomYCorner + 1;
+                        xBendCorner = endRoomXCorner;
                         hallwayVerticalLength++;
 
                         if (lShape == true)
                         {
                             int bendDir = WhichWayToBend(endingRoom, xBendCorner, yBendCorner);
-                            yCornerEnding = yBendCorner;
-                            xCornerEnding = xBendCorner + (bendDir * Mathf.Abs(endingRoom.xPos - xBendCorner));
+                            endRoomYCorner = yBendCorner;
+                            endRoomXCorner = xBendCorner + (bendDir * Mathf.Abs(endingRoom.xPos - xBendCorner));
                             hallwayHorizontalLength = endingRoom.xPos - xBendCorner;
                         }
                     }
@@ -151,8 +240,8 @@ public class Hallways {
             }
         }
 	}
-
-    public bool CheckIfHallwayOverlap(Room endRoom, int xHallwayEnd,int yHallwayEnd)
+        */
+   /** public bool CheckIfHallwayOverlap(Room endRoom, int xHallwayEnd,int yHallwayEnd)
     {
         if ((xHallwayEnd >= endRoom.xPos && xHallwayEnd <= endRoom.xPos + endRoom.roomWidth) &&
             (yHallwayEnd >= endRoom.yPos && yHallwayEnd <= endRoom.yPos + endRoom.roomHeight) )
@@ -161,9 +250,9 @@ public class Hallways {
         }
             return false;
     }
-
     public int WhichWayToBend(Room endRoom, int xBend,int yBend)
     {
+
         if (endRoom.xPos > xBend)
         {
             return 1;
@@ -180,8 +269,8 @@ public class Hallways {
         {
             return -1;
         }
-        return 0;
-    }
+        return 0; */
+    
 
    /*  public int calulateOverlap(int R1LeftCorner, int R2LeftCorner, int R1RightCorner, int R2RightCorner)
     {
@@ -200,4 +289,4 @@ public class Hallways {
         
     }*/
 
-}
+//}
--- Spacetime Guy/Assets/Scripts/boardCreator.cs
+++ Spacetime Guy/Assets/Scripts/boardCreator.cs
@@ -58,27 +58,38 @@ public class boardCreator : MonoBehaviour
             i.room = new Room(i.height, i.width, i.xpos, i.ypos);
         }
         hallwayList = new List<Hallways>();
-        
-        foreach (Leafs i in childLeafs)
+
+        //foreach (Leafs i in childLeafs)
+        for (int i = 0; i < (childLeafs.Count - 1); i++)
         {
-            int temp1 = Random.Range(0, childLeafs.Count);
-            int temp2 = Random.Range(0, childLeafs.Count);
-            while (temp1 == temp2)
-            {
-                temp1 = Random.Range(0, childLeafs.Count);
-            }
-            Hallways h1 = new Hallways(childLeafs[temp1].room, childLeafs[temp2].room);
-            hallwayList.Add(h1);
+            // int temp1 = Random.Range(0, childLeafs.Count);
+            // int temp2 = Random.Range(0, childLeafs.Count);
+            //  while (temp1 == temp2)
+            //  {
+            //temp1 = Random.Range(0, childLeafs.Count);
+            //  }
+            //if (i != null)
+            //{
+                Hallways h1 = new Hallways(childLeafs[i].room, childLeafs[i + 1].room);
+            printroom(childLeafs[i].room);
+            printroom(childLeafs[i + 1].room);
+            print(i);
+            print(i + 1);
+                hallwayList.Add(h1);
+            //}
         }
+        /**
         foreach (Leafs i in childLeafs)
         {
             printroom(i.room);
         }
         print("END OF PRINT ROOMSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+       
         foreach (Leafs i in childLeafs)
         {
             printleaf(i);
         }
+        */
         board = new int[levelHeight, levelWidth];
         for (int i = 0; i < levelWidth; i++)
         {
@@ -98,52 +109,59 @@ public class boardCreator : MonoBehaviour
                 }
             }
         }
+        print("size of HallwaysList: " + hallwayList.Count);
         foreach (Hallways i in hallwayList)
         {
+            //printHallways(i);
+            //going left
             if (i.hallwayHorizontalLength > 0)
             {
-                for (int j = i.xCornerStarting; j != i.xCornerStarting + i.hallwayHorizontalLength; j++)
+                for(int j = i.startRoomXCorner; j < i.endRoomXCorner; j++)
                 {
-                    board[j, i.yCornerStarting] = 0;
+                    board[j, i.startRoomYCorner] = 0;
                 }
             }
-            else
+         //going right
+         else if (i.hallwayHorizontalLength < 0)
             {
-                for (int j = i.xCornerStarting; j != i.xCornerStarting + i.hallwayHorizontalLength; j--)
+                for(int j = i.startRoomXCorner; j >= i.endRoomXCorner; j--)
                 {
-                    board[j, i.yCornerStarting] = 0;
+                    board[j, i.startRoomYCorner] = 0;
                 }
             }
-            if (i.hallwayVerticalLength > 0)
+         //going upward
+         else if (i.hallwayVerticalLength > 0)
             {
-                for (int j = i.yCornerStarting; j != i.yCornerStarting + i.hallwayVerticalLength; j++)
+                for (int j = i.startRoomYCorner; j < i.endRoomYCorner; j++)
                 {
-                    board[i.xCornerStarting, j] = 0;
+                    board[i.startRoomXCorner, j] = 0;
                 }
             }
-            else
+         //going downward
+         else if (i.hallwayVerticalLength < 0)
             {
-                for (int j = i.yCornerStarting; j != i.yCornerStarting + i.hallwayVerticalLength; j--)
+                for (int j = i.startRoomXCorner; j >= i.endRoomYCorner; j--)
                 {
-                    board[i.xCornerStarting, j] = 0;
-                }
-            }
-            for (int j = 0; j < levelWidth; j++)
-            {
-                for (int k = 0; k < levelHeight; k++)
-                {
-                    if (board[j, k] == 1)
-                    {
-                        Vector2 position = new Vector2(j + xBoardCorner, k + yBoardCorner);
-                        Quaternion rotation = Quaternion.Euler(0, 0, 0);
-                        Instantiate(wall, position, rotation);
-                    }
+                    board[i.startRoomXCorner, j] = 0;
                 }
             }
 
         }
+    
+        for (int j = 0; j < levelWidth; j++)
+        {
+            for (int k = 0; k < levelHeight; k++)
+            {
+                if (board[j, k] == 1)
+                {
+                    Vector2 position = new Vector2(j + xBoardCorner, k + yBoardCorner);
+                    Quaternion rotation = Quaternion.Euler(0, 0, 0);
+                    Instantiate(wall, position, rotation);
+                }
+            }
+        }
         //this.printArray(board);
-
+    //}
 
 
        
@@ -172,8 +190,8 @@ public class boardCreator : MonoBehaviour
         print("The x coordinate is " +  r.xPos);
         print("The Y coordinate is " + r.yPos);
 
-        print("The width is " + r.roomWidth);
-        print("The height is " + r.roomHeight);
+        //print("The width is " + r.roomWidth);
+        //print("The height is " + r.roomHeight);
     }
     void printleaf(Leafs l)
     {
@@ -182,6 +200,16 @@ public class boardCreator : MonoBehaviour
 
         print("The width is " + l.width);
         print("The height is " + l.height);
+    }
+    void printHallways(Hallways h)
+    {
+    
+        print("the starting xcoord of hallway is " + h.startRoomXCorner);
+        print("the ending xcoord of hallway is " + h.endRoomXCorner);
+        print("the starting ycoord of hallway is" + h.startRoomYCorner);
+        print("the ending ycoord of hallway is" + h.endRoomYCorner);
+        print("the vertical length is" + h.hallwayVerticalLength);
+        print("the horizontal length is" + h.hallwayHorizontalLength);
     }
     void printArray(int[,] board)
     {
