6a80b3a1f39dd8549d6d2b284694d7e23a7a2a02
Jesse-School\Jesse <jrr867@mail.usask.ca>
2017-11-09 11:26:46 -0600

    Started Code for an interface class for powerups; each powerup has to powerup the whatever it collides with and power down after a certain time (which may or may not be infinity)

---
+++ Spacetime Guy/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PowerUp : MonoBehaviour {
+
+   protected abstract void startPowerUp();
+
+
+   protected abstract void startPowerDown();
+ 
+}
---
+++ Spacetime Guy/Assets/Scripts/SpeedPowerUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerUp : PowerUp {
+
+	protected override void startPowerUp()
+    {
+        //OnCollision2D(Rid){
+        //
+        //}
+    }
+
+    protected override void startPowerDown()
+    {
+
+    }
+       
+}
