a010331a31e1734ca5fb15dbb66933bb766deb4f
Dominic McKeith <dominic.mckeith@usask.ca>
2017-11-09 10:57:13 -0600

    Added camera movement.

---
+++ Spacetime Guy/Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject ObjectToTrack;
+    [SerializeField]
+    private float movementSpeed;
+
+	void Update () {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(ObjectToTrack.transform.position.x, ObjectToTrack.transform.position.y, this.transform.position.z), movementSpeed * Time.deltaTime);
+        //body.velocity = ObjectToTrack.GetComponent<Rigidbody2D>().velocity - 
+    }
+}
