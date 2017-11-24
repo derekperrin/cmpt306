9f3db8eab2f8caed7efd36fadfd5c387b52f1551
Dominic McKeith <dominic.mckeith@usask.ca>
2017-11-23 20:34:15 -0600

    Added sound player. * Added crappy death sound to monsters as a demonstration.

---
+++ Spacetime Guy/Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour {
+
+    private AudioSource audioPlayer;
+
+	void Start () {
+        audioPlayer = this.GetComponent<AudioSource>();
+	}
+	
+	public void Play(AudioClip sound)
+    {
+        Debug.Log("Playing Sound...");
+        audioPlayer.PlayOneShot(sound);
+    }
+}
