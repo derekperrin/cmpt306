b65c50e613811a70c51f766a872b231f814af5a8
Jesse-School\Jesse <jrr867@mail.usask.ca>
2017-11-25 16:55:33 -0600

    Added Simple UI scripts for Pausing and Main Menu style things.

---
+++ Spacetime Guy/Assets/Scripts/UI/LoadSceneButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneButton : MonoBehaviour {
+    //when clicked, this will load the new (actual game) scene.
+    public void LoadingScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+}
---
+++ Spacetime Guy/Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    public Text timer_text;
+    public GameObject pause_menu;
+
+    public bool is_paused = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //timer_text.text = "Time: " + (int)Time.time;
+
+        if (Input.GetButtonDown("Pause"))
+        {
+            TogglePausing();
+        }
+
+    }
+    public void TogglePausing()
+    {
+        is_paused = !is_paused;
+
+        if (is_paused)
+        {
+            //pause the game
+            PauseGame();
+        }
+        else
+        {
+            ResumeGame();
+            //resume the game
+        }
+    }
+    public void PauseGame()
+    {
+        pause_menu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void ResumeGame()
+    {
+        pause_menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+
+
+}
