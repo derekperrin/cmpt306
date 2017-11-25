77c2bd2e405c274c72a519d0fd7b728884331e05
Dominic McKeith <dominic.mckeith@usask.ca>
2017-11-24 19:20:45 -0600

    Small changes to monster AI.

--- Spacetime Guy/Assets/Scripts/characters/MonsterWithAI.cs
+++ Spacetime Guy/Assets/Scripts/characters/MonsterWithAI.cs
@@ -6,6 +6,8 @@ public class MonsterWithAI : Monster {
     [SerializeField] private float AI_playerSearchDistance;
     private Vector3 spawnLocation;
     [SerializeField] private float AI_updateLength;
+    private float nextUpdate;
+    private bool playerFound;
 
     private Vector3 currentTarget;
 
@@ -13,11 +15,19 @@ public class MonsterWithAI : Monster {
     {
         base.Start(); // Call the parent start method.
         spawnLocation = this.gameObject.transform.position; // does this copy a reference or a value?
+        currentTarget = spawnLocation;
+        nextUpdate = Time.time + AI_updateLength;
+        playerFound = false;
     }
 
 	protected override void Movement()
     {
-        UpdateTarget();
+        if (playerToKill == null) return;
+        if (playerFound || nextUpdate <= Time.time)
+        {
+            UpdateTarget();
+            nextUpdate = Time.time + AI_updateLength;
+        }
         characterRigidBody.velocity = (currentTarget - this.gameObject.transform.position).normalized * this.characterSpeed;
     }
 	
@@ -26,10 +36,12 @@ public class MonsterWithAI : Monster {
         if (Mathf.Max(Mathf.Abs(this.transform.position.x - playerToKill.transform.position.x), Mathf.Abs(this.transform.position.y - playerToKill.transform.position.y)) <= AI_playerSearchDistance)
         {
             currentTarget = playerToKill.transform.position;
+            playerFound = true;
         }
         else
         {
             currentTarget = spawnLocation;
+            playerFound = false;
         }
     }
 }
