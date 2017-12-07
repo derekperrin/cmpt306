296df9ddb4a2c47746999ea6623190b362b0e92c
Derek Perrin <dmp450@mail.usask.ca>
2017-12-07 02:06:24 -0600

    Update Pause.cs from UI.
    * Does this because there's some serious troubles with the UI branch

--- Spacetime Guy/Assets/Scripts/UI/Pause.cs
+++ Spacetime Guy/Assets/Scripts/UI/Pause.cs
@@ -5,50 +5,56 @@ using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
-   // public Text timer_text;
-    public GameObject pause_menu;
+    [SerializeField]
+    private GameObject Children;
 
-    public bool is_paused = false;
+    public Text timer_text;
+    public float timescale;
+
+    // public bool is_paused = false;
     // Use this for initialization
     void Start()
     {
-        //ResumeGame();
+        ResumeGame();
     }
 
     // Update is called once per frame
     void Update()
     {
         //timer_text.text = "Time: " + (int)Time.time;
-
+        timescale = Time.timeScale;
         if (Input.GetButtonDown("Pause"))
         {
             TogglePausing();
         }
 
     }
+
     public void TogglePausing()
     {
-        is_paused = !is_paused;
+        // is_paused = !is_paused;
 
-        if (is_paused)
+        if (Time.timeScale == 1)
         {
-            //pause the game
             PauseGame();
         }
         else
         {
             ResumeGame();
-            //resume the game
         }
     }
     public void PauseGame()
     {
-        pause_menu.SetActive(true);
+        Children.SetActive(true);
         Time.timeScale = 0;
+        Debug.Log(Time.timeScale.ToString());
     }
     public void ResumeGame()
     {
-        pause_menu.SetActive(false);
+        Children.SetActive(false);
         Time.timeScale = 1;
     }
+
+
+
 }
